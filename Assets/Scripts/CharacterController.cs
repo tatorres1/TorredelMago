c59ecef9b0f61807423c827dfcf6aba6c06704bc
Thalia Torres <torres.thalia31@gmail.com>
2024-07-30 19:44:19 -0500

    jugadore movimientos

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float runSpeed = 4;
+    public float rotationSpeed = 250;
+    public Animator animator;
+    private float x, y;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime*rotationSpeed, 0);
+        transform.Translate(0,0, y * Time.deltaTime*runSpeed);
+
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY",y);
     }
 }
