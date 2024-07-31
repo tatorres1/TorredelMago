9cb031e8b88c4948cc1b183186d6b3cd3fa5e178
Thalia Torres <torres.thalia31@gmail.com>
2024-07-31 07:17:18 -0500

    nuevos cambios multiplayer

--- Assets/Scripts/Collective.cs
+++ Assets/Scripts/Collective.cs
@@ -7,13 +7,21 @@ public class Collective : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
+        // Verificar si el objeto que entró en el trigger tiene el tag "Player"
         if (other.CompareTag("Player"))
         {
-            Player player = other.GetComponent<Player>();
+            // Obtener el componente Player del objeto que colisionó
+            NewBehaviourScript player = other.GetComponent<NewBehaviourScript>();
             if (player != null)
             {
-                //player.CollectItem();
+                // Llamar al método CollectItem del jugador
+                player.CollectItem();
+                // Destruir el objeto
                 Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogError("Player component not found on the detected object.");
             }
         }
     }
